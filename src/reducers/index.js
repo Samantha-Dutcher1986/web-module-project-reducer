d72c71f146db5831269c8399b596452023349c7f
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2021-05-22 22:28:01 -0500

    finished

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, ADD_TO_MEMORY, RECALL, CLEAR_MEMORY } from './../actions';
 
 export const initialState = {
     total: 100,
@@ -7,6 +7,7 @@ export const initialState = {
 }
 
 const calculateResult = (num1, num2, operation) => {
+    console.log( 'all the things', state, num1, num2, operation )
     switch(operation) {
         case("+"):
             return num1 + num2;
@@ -14,6 +15,8 @@ const calculateResult = (num1, num2, operation) => {
             return num1 * num2;
         case("-"):
             return num1 - num2;
+            default:
+        return state;
     }
 }
 
@@ -26,9 +29,10 @@ const reducer = (state, action) => {
             });
 
         case(APPLY_NUMBER):
+        console.log(action.payload)
             return ({ 
                 ...state, 
-                total: calculateResult(state.total, action.payload, state.operation)
+                total: calculateResult(state, state.total, action.payload, state.operation)
             });
         
         case(CHANGE_OPERATION):
@@ -37,6 +41,20 @@ const reducer = (state, action) => {
                 operation: action.payload
             });
             
+        case(CLEAR_DISPLAY):
+        return({
+            ...state,
+            total: 0
+        });
+
+        case(ADD_TO_MEMORY):
+        return({...state, memory:state.total});
+
+        case(RECALL):
+        return({...state, total: state.memory});
+
+        case (CLEAR_MEMORY):
+        return({...state, memory: 0})
         default:
             return state;
     }
